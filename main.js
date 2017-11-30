906b1e74de26f2f9169adc9b5b5504dd4bcabdc7
ThomasDelap <dbmagouille@gmail.com>
2017-11-30 12:12:51 +0100

    Finish

---
+++ main.js
@@ -0,0 +1,10 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+let mainWindow;
+
+app.on('ready', () => {
+
+    let mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow.loadURL(`file://${__dirname}/dist/index.html`)
+});
